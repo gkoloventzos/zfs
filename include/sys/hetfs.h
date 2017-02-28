a3f0a72c41c2e94e883798cdeb5e44338c036209
Georgios Koloventzos <g.koloventzos@gmail.com>
2017-02-28 08:48:26 +0000

    no changes in kernel tree

---
+++ include/sys/hetfs.h
@@ -0,0 +1,25 @@
+#ifndef HETFS_H
+#define HETFS_H
+
+#define MAX_DIFF 200000000
+#define MAX_NAME 255
+
+/*
+ * Five categories, from faster to slower:
+ *
+ * nonrot (SSD)      disk or mirror
+ * nonrot (SSD)      raidz
+ * mixed nonrot+rot  anything (raidz makes little sense)
+ * rot (HDD)         disk or mirror
+ * rot (HDD)         raidz
+ */
+
+#define	METASLAB_ROTOR_VDEV_TYPE_SSD		0x01
+#define	METASLAB_ROTOR_VDEV_TYPE_SSD_RAIDZ	0x02
+#define	METASLAB_ROTOR_VDEV_TYPE_MIXED		0x04
+#define	METASLAB_ROTOR_VDEV_TYPE_HDD		0x08
+#define	METASLAB_ROTOR_VDEV_TYPE_HDD_RAIDZ	0x10
+
+#define filp2name(filp) filp->f_path.dentry->d_name.name
+
+#endif
