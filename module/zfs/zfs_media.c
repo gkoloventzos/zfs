a4e3f7ea69a032f30065712c4cb8312faa6f250b
Georgios Koloventzos <g.koloventzos@gmail.com>
2017-11-21 17:28:40 +0100

    interval list seems to working correct

--- module/zfs/zfs_media.c
+++ module/zfs/zfs_media.c
@@ -35,6 +35,18 @@
 #include <sys/zfs_media.h>
 #include <linux/slab.h>
 
+/*
+ *              0 1 3 2            -1
+ *              | | | |             |
+ *              v v v v             v
+ * head -> ( ) -> (   ) -> ( ) -> NULL
+ * Position of the offset in the list
+ * 0 : between the 2 nodes
+ * 1 : Equal with the start of the node
+ * 2 : Equal with the end of the node
+ * 3 : Inside the node
+ * -1 : when it is after the last node. For partial reads mostly.
+ */
 medium_t *
 find_in(struct list_head *head, medium_t *start, bool contin, loff_t posi, int *ret) {
 
@@ -77,142 +89,117 @@ find_in(struct list_head *head, medium_t *start, bool contin, loff_t posi, int *
 medium_t *
 zfs_media_add(struct list_head *dn, loff_t ppos, size_t len, int8_t rot)
 {
-
     int start, stop;
     medium_t *new, *loop, *next, *del;
+    struct medium *posh, *nh;
     loff_t end = ppos + len;
-    loop = new = del = NULL;
+    loop = new = del = next = NULL;
     start = stop = -1;
 
-    if (list_empty(dn)) {
-        new = kzalloc(sizeof(medium_t), GFP_KERNEL);
-        if (new == NULL)
-            return NULL;
-        new->m_start = ppos;
-        new->m_end = end;
-        new->m_type = rot;
-        list_add_tail(&new->list, dn);
-        return new;
-    }
-    return NULL;
-    loop = find_in(dn, list_first_entry(dn, typeof(*new), list), false, ppos, &start);
-
     new = kzalloc(sizeof(medium_t), GFP_KERNEL);
-    if (new == NULL)
+    if (new == NULL) {
+        printk(KERN_EMERG "[ERROR][ZFS_MEDIA_ADD]Cannot allocate for new medium\n");
         return NULL;
+    }
     new->m_start = ppos;
     new->m_end = end;
     new->m_type = rot;
-
-    if (loop == NULL) {
-        list_add_tail(dn, &new->list);
+    if (list_empty(dn)) {
+        list_add_tail(&new->list, dn);
         return new;
     }
 
+    /* Find where the begining of this part is supposed to be added.*/
+    loop = find_in(dn, list_first_entry(dn, typeof(*new), list), false, ppos, &start);
+
+    /* Probably on read. Reading 10 first and then 10 last lines.
+     * Accessing not sequencial parts of file.*/
+    if (loop == NULL) {
+        list_add_tail(&new->list, dn);
+        return new;
+    }
+
+    /* Find where the end of this part is supposed to be added.*/
     del = find_in(dn, loop, true, end, &stop);
 
-    if (loop == del && loop != NULL) {
-        if (start != 0 && stop != 0) {
-            if (loop->m_type != rot) {
-                list_add(&new->list, &loop->list);
-                del = kzalloc(sizeof(medium_t), GFP_KERNEL);
-                if (del == NULL)
-                    return NULL;
-                del->m_start = end;
-                del->m_end = loop->m_end;
-                del->m_type = loop->m_type;
-                loop->m_end = ppos;
-                list_add(&new->list, &del->list);
-                return new;
-            }
-            else {
-                kzfree(new);
-                return loop;
-            }
-        }
-    }
 
     switch(start) {
         case 0:
-            list_add_tail(&new->list, &loop->list);
+        case 2:
+            list_add(&new->list, loop->list.prev);
             next = loop;
             break;
         case 1:
-            if (loop->m_type == rot) {
-                new->m_start = loop->m_start;
-                list_add_tail(&new->list, &loop->list);
-                next = loop;
-            }
-            else {
+            if (rot != loop->m_type) {
                 list_add(&new->list, &loop->list);
                 next = list_next_entry(new, list);
             }
-            break;
-        case 2:
-            list_add_tail(&new->list, &loop->list);
-            next = loop;
+            else {
+                loop->m_end = end;
+                next = list_next_entry(loop, list);
+            }
             break;
         case 3:
-            if (loop->m_type == rot) {
-                loop->m_end = end;
-                kzfree(new);
-                new = loop;
-                next = list_next_entry(new, list);
-            }
-            else {
-                loop->m_end = new->m_start;
+            if (rot != loop->m_type) {
+                loop->m_end = ppos;
                 list_add(&new->list, &loop->list);
                 next = list_next_entry(new, list);
             }
             break;
         default:
+            printk(KERN_EMERG "[ERROR][ZFS_MEDIA_ADD]Default in start.\n");
             return NULL;
     }
-    while (next != del) {
+    /* Remove the excess part */
+    while (next != NULL && next != del && &next->list != (dn)) {
         loop = list_next_entry(next, list);
         list_del(&next->list);
         kzfree(next);
         next = loop;
     }
-    switch (stop) {
+
+    /* After last node */
+    if (del == NULL) {
+        new = list_last_entry(dn, typeof(*new), list);
+        new->m_end = end;
+        return new;
+    }
+
+    switch(stop) {
         case 0:
-            break;
+            loop = list_entry(del->list.prev, typeof(*loop), list);
+            loop->m_end = end;
+            return loop;
         case 1:
-            loop = list_next_entry(del, list);
-            if (loop == NULL) {
-                list_del(&del->list);
-                kzfree(del);
-                break;
-            }
-            if (end < loop->m_start) {
-                return new;
-            }
-            /*only == */
-            if (loop->m_type == rot) {
-                new->m_end = loop->m_end;
-                list_del(&loop->list);
-                kzfree(loop);
-            }
-            break;
+            del->m_type = rot;
+            return del;
         case 2:
-            if (del->m_type == rot) {
-                new->m_end = del->m_end;
-                list_del(&del->list);
-                kzfree(del);
-            }
+            loop = list_entry(del->list.prev, typeof(*loop), list);
+            loop->m_type = rot;
+            loop->m_end = end;
+            return loop;
             break;
         case 3:
-            if (del->m_type == rot) {
-                new->m_end = del->m_end;
-                list_del(&del->list);
-                kzfree(del);
-            }
+            if (del->m_type == rot)
+                return del;
             else {
-                del->m_start = end;
+                new = kzalloc(sizeof(medium_t), GFP_KERNEL);
+                if (new == NULL) {
+                    printk(KERN_EMERG "[ERROR][ZFS_MEDIA_ADD]Cannot allocate for new medium\n");
+                    return NULL;
+                }
+                new->m_start = end;
+                new->m_end = del->m_end;
+                new->m_type = del->m_type;
+                del->m_end = end;
+                del->m_type = rot;
+                list_add(&new->list, &del->list);
+                return del;
             }
             break;
         default:
-            break;
+            printk(KERN_EMERG "[ERROR][ZFS_MEDIA_ADD]Default in stop.\n");
+            return NULL;
     }
 
     return new;
